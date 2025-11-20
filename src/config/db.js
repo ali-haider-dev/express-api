217d27a7e2ae5f65d35840cb8cdf8dcf071ed8bc
ali-haider-dev <alihaider061297@gmail.com>
2025-11-19 21:46:19 -0800

    Optimize DB connection for serverless

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  // Check if we have an active connection
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    // In serverless, we shouldn't exit the process, just throw the error
+    // process.exit(1); 
   }
 };
 
